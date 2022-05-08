c3ac0c23196c803ba0d7d8ba13edee557cbac126
Yuchen Zhong <yuchen21@fb.com>
2022-05-08 16:27:03 -0400

    Add Server Reflection

--- build.rs
+++ build.rs
@@ -1,9 +1,15 @@
+use std::{env, path::PathBuf};
+
 fn main() {
     let proto_file = "./proto/book_store.proto";
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+
     tonic_build::configure()
         .build_server(true)
+        .file_descriptor_set_path(out_dir.join("greeter_descriptor.bin"))
         .out_dir("./src")
         .compile(&[proto_file], &["."])
         .unwrap_or_else(|e| panic!("protobuf compile error: {}", e));
+
     println!("cargo:rerun-if-changed={}", proto_file);
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,10 @@ use bookstore::{GetBookRequest, GetBookResponse};
 
 mod bookstore {
     include!("bookstore.rs");
+
+    // Add this
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("greeter_descriptor");
 }
 
 
@@ -35,10 +39,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse().unwrap();
     let bookstore = BookStoreImpl::default();
 
+    // Add this
+    let reflection_service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(bookstore::FILE_DESCRIPTOR_SET)
+        .build()
+        .unwrap();
+
     println!("Bookstore server listening on {}", addr);
 
     Server::builder()
         .add_service(BookstoreServer::new(bookstore))
+        .add_service(reflection_service) // Add this
         .serve(addr)
         .await?;
 
