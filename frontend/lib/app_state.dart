571f261faabeb6c8bcd27c353dff482f13516940
Jon Latane <jonlatane@gmail.com>
2022-09-08 16:43:15 -0400

    things

---
+++ frontend/lib/app_state.dart
@@ -0,0 +1,62 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:jonline/db.dart';
+import 'package:jonline/router/auth_guard.dart';
+import 'package:jonline/router/router.gr.dart';
+import 'package:provider/provider.dart';
+
+import 'main.dart';
+import 'models/jonline_account.dart';
+
+const animationDuration = Duration(milliseconds: 300);
+
+class AppState extends State<MyApp> {
+  final authService = AuthService();
+  ValueNotifier<List<JonlineAccount>> accounts = ValueNotifier([]);
+
+  final _rootRouter = RootRouter(
+    authGuard: AuthGuard(),
+  );
+
+  updateAccountList() async {
+    print("updateAccountList");
+    accounts.value = await JonlineAccount.accounts;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // JonlineAccount.setupStorage(callback: updateAccountList);
+    Future.microtask(updateAccountList);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      theme: ThemeData.dark().copyWith(
+        // bottomAppBarColor: const Color(0xFF884DF2),
+        // splashColor: const Color(0xFFFFC145),
+        // buttonColor: const Color(0xFF884DF2),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF2E86AB)),
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.macOS: NoShadowCupertinoPageTransitionsBuilder(),
+          TargetPlatform.iOS: NoShadowCupertinoPageTransitionsBuilder(),
+          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+        }),
+      ),
+      routerDelegate: _rootRouter.delegate(
+        navigatorObservers: () => [AutoRouteObserver()],
+      ),
+      // routeInformationProvider: _rootRouter.routeInfoProvider(),
+      routeInformationParser: _rootRouter.defaultRouteParser(),
+      builder: (_, router) {
+        return ChangeNotifierProvider<AuthService>(
+          create: (_) => authService,
+          child: BooksDBProvider(
+            child: router!,
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ frontend/lib/models/server_errors.dart
@@ -0,0 +1,36 @@
+import 'package:grpc/grpc.dart';
+
+final grpcErrorConversions = {
+  RegExp(r'^([\w]+)_too_short_min_?([\d]+)$'): (matches) =>
+      "${_capitalize(matches[1])} must be at least ${matches[2]} character${_sIfPlural(matches[2])}.",
+  RegExp(r'^([\w]+)_too_long_max_?([\d]+)$'): (matches) =>
+      "${_capitalize(matches[1])} must be less than ${matches[2]} character${_sIfPlural(matches[2])}.",
+  RegExp(r'^[\w]+$'): (matches) =>
+      "${_capitalize(matches[0]).replaceAll('_', ' ')}.",
+};
+
+String formatServerError(Object e) {
+  if (e is GrpcError) {
+    return formatGrpcError(e);
+  }
+  return "An unknown error occurred.";
+}
+
+String formatGrpcError(GrpcError e) {
+  if (e.message == null) {
+    return "No message was included in the error.";
+  }
+  final message = e.message!;
+  String? formattedMessage;
+  grpcErrorConversions.forEach((expression, handler) {
+    if (formattedMessage == null && expression.hasMatch(message)) {
+      final match = expression.firstMatch(message)!;
+      final groups = List<int>.generate(match.groupCount + 1, (i) => i);
+      formattedMessage = handler(match.groups(groups));
+    }
+  });
+  return formattedMessage ?? "Error message: $message";
+}
+
+_sIfPlural(s, {suffix = 's'}) => s == '1' ? '' : suffix;
+_capitalize(String s) => s[0].toUpperCase() + s.substring(1);
