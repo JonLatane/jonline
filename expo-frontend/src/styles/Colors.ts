c601e894d2640bbfea1de5e5dc54a602793f796c
Jon Latane <jonlatane@gmail.com>
2023-01-21 16:59:29 -0500

    remove expo-frontend

--- expo-frontend/src/styles/Colors.ts
+++
@@ -1,4 +0,0 @@
-export const PRIMARY = "#DAFEB7";
-export const SECONDARY = "#ACC18A";
-export const DARK = "#605B56";
-export const LIGHT = "#F2FBE0";
--- expo-frontend/src/types.ts
+++
@@ -1,4 +0,0 @@
-export interface FunFact {
-  fact: string;
-  rating: 1 | -1 | 0;
-}
