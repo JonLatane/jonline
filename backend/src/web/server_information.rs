5cc823902cfbc6b566881be77c8603e0775f2f83
Jon Latane <jonlatane@gmail.com>
2023-06-07 21:51:27 -0400

    Jonline CI

--- backend/src/web/server_information.rs
+++ backend/src/web/server_information.rs
@@ -1,14 +1,15 @@
 use super::RocketState;
 use crate::rpcs::{get_server_configuration, get_service_version};
-use rocket::{response::Redirect, routes, Route, State};
 use percent_encoding::{utf8_percent_encode, NON_ALPHANUMERIC};
+use rocket::{response::Redirect, routes, Route, State};
+use rocket_cache_response::CacheResponse;
 
 lazy_static! {
     pub static ref INFORMATIONAL_PAGES: Vec<Route> = routes![info_shield];
 }
 
 #[rocket::get("/info_shield")]
-async fn info_shield(state: &State<RocketState>) -> Redirect {
+async fn info_shield(state: &State<RocketState>) -> CacheResponse<Redirect> {
     let service_version = get_service_version().unwrap().version;
 
     let mut conn = state.pool.get().unwrap();
@@ -20,8 +21,8 @@ async fn info_shield(state: &State<RocketState>) -> Redirect {
         .unwrap_or("Jonline".to_string());
     let encoded_server_name = utf8_percent_encode(&server_name, NON_ALPHANUMERIC);
 
-    Redirect::temporary(format!(
+    CacheResponse::NoCache(Redirect::temporary(format!(
         "https://img.shields.io/badge/{}-{}-informational?style=for-the-badge",
         encoded_server_name, service_version
-    ))
+    )))
 }
