e0d6e536a758d97b925911e81f4a930b6f202cac
Jon Latane <jonlatane@gmail.com>
2024-01-17 23:25:04 -0500

    hide locks

--- frontends/tamagui/packages/app/features/navigation/use_hide_navigation.ts
+++ frontends/tamagui/packages/app/features/navigation/use_hide_navigation.ts
@@ -2,24 +2,39 @@ import { useMedia } from '@jonline/ui';
 import useDetectScroll, { Axis, Direction } from '@smakss/react-scroll-direction';
 import { useAppDispatch, useLocalConfiguration } from 'app/hooks';
 import { setShowPinnedServers } from 'app/store';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export function useHideNavigation() {
   const mediaQuery = useMedia();
   const { scrollDir, scrollPosition } = useDetectScroll({
-    thr: 100,
+    thr: 30,
     axis: Axis.Y,
     scrollUp: Direction.Up,
     scrollDown: Direction.Down,
     still: Direction.Still
   })
   const { showPinnedServers } = useLocalConfiguration();
-  const doHide = mediaQuery.xShort && scrollDir === Direction.Down;
+  const [hide, setHide] = useState(false);
+  const [hideLock, setHideLock] = useState(false);
+  // const doHide = mediaQuery.xShort && scrollDir === Direction.Down;
   const dispatch = useAppDispatch();
   useEffect(() => {
-    if (doHide && showPinnedServers) {
+    if (!hideLock) {
+      if (!hide && mediaQuery.xShort && scrollDir === Direction.Down) {
+        setHideLock(true);
+        setHide(true);
+        setTimeout(() => setHideLock(false), 1000);
+      } else if (hide && mediaQuery.xShort && scrollDir === Direction.Up) {
+        setHideLock(true);
+        setHide(false);
+        setTimeout(() => setHideLock(false), 1000);
+      }
+    }
+  }, [mediaQuery.xShort, scrollDir, hide, hideLock]);
+  useEffect(() => {
+    if (hide && showPinnedServers) {
       dispatch(setShowPinnedServers(false));
     }
-  }, [doHide, showPinnedServers]);
-  return doHide;
+  }, [hide, showPinnedServers]);
+  return hide;
 }
