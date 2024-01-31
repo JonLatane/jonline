9df0ac8ec3a89eff68780ebe76eac605307e14a3
Jon Latane <jonlatane@gmail.com>
2024-01-31 16:51:16 -0500

    reply area things

--- frontends/tamagui/packages/app/features/post/reply_area.tsx
+++ frontends/tamagui/packages/app/features/post/reply_area.tsx
@@ -1,5 +1,5 @@
 import { Permission, Post } from '@jonline/api'
-import { Button, Heading, ScrollView, TextArea, Tooltip, XStack, YStack, ZStack, isClient, isWeb, useTheme, useToastController, useWindowDimensions } from '@jonline/ui'
+import { Button, Heading, ScrollView, TextArea, Tooltip, XStack, YStack, ZStack, isClient, isWeb, reverseStandardAnimation, useTheme, useToastController, useWindowDimensions } from '@jonline/ui'
 import { ChevronRight, Edit3 as Edit, Eye, Send as SendIcon } from '@tamagui/lucide-icons'
 import { TamaguiMarkdown } from 'app/components'
 import { useAccountOrServerContext } from 'app/contexts'
@@ -13,6 +13,7 @@ import { MediaRef } from '../media/media_chooser'
 import { PostMediaManager } from './post_media_manager'
 import { PostMediaRenderer } from './post_media_renderer'
 import { useHideNavigation } from '../navigation/use_hide_navigation'
+import { themedButtonBackground } from 'app/utils'
 
 interface ReplyAreaProps {
   replyingToPath: string[];
@@ -20,7 +21,6 @@ interface ReplyAreaProps {
   onStopReplying?: () => void;
 }
 
-// Must be within an AccountOrServerContextProvider
 export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, onStopReplying }) => {
   const dispatch = useAppDispatch();
   const currentAccountOrServer = useAccountOrServer();
@@ -31,12 +31,11 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
 
   const [replyText, setReplyText] = useState('');
   const [previewReply, setPreviewReply] = useState(false);
-  const maxPreviewHeight = useWindowDimensions().height * 0.3;
-  // const [isReplying, setIsReplying] = useState(false);
   const [isSendingReply, setIsSendingReply] = useState(false);
-  const textAreaRef = React.createRef<TextInput>();// as React.MutableRefObject<HTMLElement | View>;
+  const textAreaRef = React.createRef<TextInput>();
   const chatUI = useRootSelector((state: RootState) => state.app.discussionChatUI);
   const [showMedia, setShowMedia] = useState(true);
+  const maxPreviewHeight = (useWindowDimensions().height - 80 - (showMedia && media.length > 0 ? 100 : 0)) * 0.5;
 
   const [replyTextFocused, _setReplyTextFocused] = useState(false);
   const [hasReplyTextFocused, setHasReplyTextFocused] = useState(false);
@@ -66,7 +65,6 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
         toast.show('Failed to send reply.');
         return;
       }
-      // setIsReplying(false);
       setHasReplyTextFocused(false);
       setMedia([]);
       setReplyText('');
@@ -75,32 +73,20 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
       }
     });
   }
-  // const replyingToPost = 
   let pathIndex = 0;
   const rootPost = useRootSelector((state: RootState) => selectPostById(state.posts, replyingToPath[pathIndex++]!));
   const targetPostId = replyingToPath[replyingToPath.length - 1];
   let targetPost = rootPost as Post | undefined;
   while (targetPost != null && targetPost?.id != targetPostId) {
     const replyId = replyingToPath[pathIndex++];
-    // debugger;
     targetPost = targetPost?.replies?.find(reply => reply.id == replyId);
-    // debugger;
   }
   const replyingToPost = targetPost;
-  // const sendReplyStatus = useRootSelector((state: RootState) => state.posts.sendReplyStatus);
   useEffect(() => {
-    // if (isSendingReply && sendReplyStatus == 'sent') {
-    //   setIsSendingReply(false);
-    //   setReplyText('');
-    //   dispatch(confirmReplySent!());
-    //   if (chatUI && isClient) {
-    //     window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
-    //   }
-    // }
-    if (!isSendingReply && previewReply && replyText == '' && media.length == 0) {
+    if (previewReply && replyText == '') {
       setPreviewReply(false);
     }
-  }, [isSendingReply, previewReply, replyText, media.length]);
+  }, [previewReply, replyText]);
   const canSend = replyText.length > 0 || media.length > 0;
   const canComment = (accountOrServer.account?.user?.permissions?.includes(Permission.REPLY_TO_POSTS)
     || accountOrServer.account?.user?.permissions?.includes(Permission.CREATE_POSTS));
@@ -118,22 +104,10 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
             <Heading size='$1' f={1}>Media {media.length > 0 ? `(${media.length})` : undefined}</Heading>
           </Button>
           {showMedia
-            ? 
-            // previewReply
-            //   ? <PostMediaRenderer {...{
-            //     post: Post.create({
-            //       // ...post,
-            //       id: '',
-            //       media,
-            //       embedLink
-            //     })
-            //   }} />
-            //   : 
-              <PostMediaManager
-                link={''}
-                {...{ media, setMedia, embedLink, setEmbedLink }}
-                disableInputs={isSendingReply}
-              />
+            ? <PostMediaManager
+              {...{ media, setMedia, embedLink, setEmbedLink }}
+              disableInputs={isSendingReply}
+            />
             : undefined}
         </>
         : undefined}
@@ -144,16 +118,15 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
           <XStack f={1} />
         </XStack>
         : undefined}
-        {previewReply
-          ? <YStack p='$3' f={1} backgroundColor='$background'
-          >
-            <ScrollView maxHeight={maxPreviewHeight}>
-              <TamaguiMarkdown text={replyText} shrink />
-            </ScrollView>
-          </YStack>
-          : undefined}
+      {previewReply
+        ? <YStack px='$3' py='$1' f={1} backgroundColor='$background' animation='standard' {...reverseStandardAnimation}>
+          <ScrollView maxHeight={maxPreviewHeight}>
+            <TamaguiMarkdown text={replyText} shrink />
+          </ScrollView>
+        </YStack>
+        : undefined}
       <XStack>
-        <YStack f={1}>
+        <YStack f={1} mt='$1'>
           <TextArea f={1} value={replyText} ref={textAreaRef}
             disabled={isSendingReply} opacity={isSendingReply || !canSend ? 0.5 : 1}
             onChangeText={t => setReplyText(t)}
@@ -161,27 +134,13 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
             onBlur={() => setReplyTextFocused(false)}
             placeholder={`Reply to this post. Markdown is supported.`} />
         </YStack>
-        {/* <ZStack f={1}>
-          <TextArea f={1} value={replyText} ref={textAreaRef}
-            disabled={isSendingReply} opacity={isSendingReply || !canSend ? 0.5 : 1}
-            onChangeText={t => setReplyText(t)}
-            onFocus={() => setReplyTextFocused(true)}
-            onBlur={() => setReplyTextFocused(false)}
-            placeholder={`Reply to this post. Markdown is supported.`} />
-          {previewReply
-            ? <YStack p='$3' f={1} backgroundColor='$background'>
-              <ScrollView maxHeight={maxPreviewHeight} height={maxPreviewHeight}>
-                <TamaguiMarkdown text={replyText} />
-              </ScrollView>
-            </YStack>
-            : undefined}
-        </ZStack> */}
         <YStack mr='$2' ml='$2' mt='auto' ac='flex-end' >
           <YStack f={1} />
           <Tooltip placement="top-end" key={`preview-button-${previewReply}`}>
             <Tooltip.Trigger>
-              <Button circular mb='$2' icon={previewReply ? Edit : Eye}
-                backgroundColor={navColor} color={navTextColor}
+              <Button circular mb='$2' icon={Eye}
+                {...themedButtonBackground(previewReply ? navColor : undefined, previewReply ? navTextColor : undefined)}
+                // backgroundColor={navColor} color={navTextColor}
                 disabled={!canSend} opacity={!canSend ? 0.5 : 1}
                 onPress={() => {
                   setPreviewReply(!previewReply);
@@ -191,7 +150,7 @@ export const ReplyArea: React.FC<ReplyAreaProps> = ({ replyingToPath, hidden, on
                 }} />
             </Tooltip.Trigger>
             <Tooltip.Content>
-              <Heading size='$2'>{previewReply ? 'Edit reply' : 'Preview reply'}</Heading>
+              <Heading size='$2'>{previewReply ? 'Hide Preview' : 'Show Preview'}</Heading>
             </Tooltip.Content>
           </Tooltip>
           {/* <YStack f={1}/> */}
