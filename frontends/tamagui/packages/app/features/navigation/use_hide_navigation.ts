d78bab853b70d17d9811142a68e8d907e6ed6440
Jon Latane <jonlatane@gmail.com>
2024-04-19 17:19:46 -0400

    fix autohide bug

--- frontends/tamagui/packages/app/features/navigation/use_hide_navigation.ts
+++ frontends/tamagui/packages/app/features/navigation/use_hide_navigation.ts
@@ -13,18 +13,24 @@ export function useHideNavigation() {
     scrollDown: Direction.Down,
     still: Direction.Still
   })
-  const { showPinnedServers, autoHideNavigation, hideNavigation: hideNavigationSetting } = useLocalConfiguration();
-  const autoHide = mediaQuery.xShort || autoHideNavigation;
+  const dispatch = useAppDispatch();
+  const { showPinnedServers, autoHideNavigation: autoHideSetting, hideNavigation } = useLocalConfiguration();
+  const forceAutoHide = mediaQuery.xShort;
+  const autoHide = forceAutoHide || autoHideSetting;
   const [hideLock, setHideLock] = useState(false);
   // const [hide, _setHide] = useState(false);
-  const hide = hideNavigationSetting;
+  // const hideNavigation = hideNavigation;
+  useEffect(() => {
+    if (hideNavigation && !forceAutoHide && !autoHideSetting) {
+      dispatch(setHideNavigation(false));
+    }
+  }, [hideNavigation, mediaQuery.xShort]);
   function setHide(value: boolean) {
     setHideLock(true);
     dispatch(setHideNavigation(value));
     setTimeout(() => setHideLock(false), 1000);
   }
   const [lastScrollDir, setLastScrollDir] = useState(Direction.Still);
-  const dispatch = useAppDispatch();
   useEffect(() => {
     if (!hideLock) {
       const atTop = scrollPosition.top === 0;
@@ -32,25 +38,25 @@ export function useHideNavigation() {
       const isUp = scrollDir === Direction.Up;
       const isDown = scrollDir === Direction.Down;
 
-      if (!hide && autoHide && isDown && directionChanged) {
+      if (!hideNavigation && autoHide && isDown && directionChanged) {
         setHide(true);
-      } else if (hide && autoHide && !mediaQuery.xShort && (isUp || atTop)) {
+      } else if (hideNavigation && autoHide && !mediaQuery.xShort && (isUp || atTop)) {
         setHide(false);
       }
     }
     setLastScrollDir(scrollDir);
-  }, [autoHide, scrollDir, lastScrollDir, hide, hideLock, scrollPosition]);
+  }, [autoHide, scrollDir, lastScrollDir, hideNavigation, hideLock, scrollPosition]);
 
-  const hideDebounce = useDebounceValue(hide, 1000);
+  const hideDebounce = useDebounceValue(hideNavigation, 1000);
   useEffect(() => {
     if (hideDebounce && showPinnedServers) {
       dispatch(setShowPinnedServers(false));
     }
-    if (hideDebounce !== hideNavigationSetting) {
+    if (hideDebounce !== hideNavigation) {
       dispatch(setHideNavigation(hideDebounce));
     }
   }, [hideDebounce]);
 
 
-  return hide;
+  return hideNavigation;
 }
