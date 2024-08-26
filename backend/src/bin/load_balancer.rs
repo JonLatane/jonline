9c35c065218cffbfd028ffcf42541bdde78f8566
Jon Latane <jonlatane@gmail.com>
2024-08-26 12:14:26 -0400

    no secrets again

--- backend/src/bin/load_balancer.rs
+++ backend/src/bin/load_balancer.rs
@@ -219,27 +219,27 @@ async fn load_config(options: &Options) -> io::Result<JonlineServerConfig> {
         log::info!("Secrets URL: {:?}", secrets_url);
         log::info!("K8s Secrets Token: {:?}", k8s_token);
 
-        let secrets = client
-            .get(secrets_url)
-            .send()
-            .await
-            .map_err(|e| {
-                log::error!("Failed to fetch secrets: {:?}", e);
-                io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    format!("Failed to fetch secrets: {:?}", e),
-                )
-            })?
-            .text()
-            .await
-            .map_err(|e| {
-                log::error!("Failed to read secrets: {:?}", e);
-                io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    format!("Failed to read secrets: {:?}", e),
-                )
-            })?;
-        log::info!("Secrets response for server {:?}: {:?}", server, secrets);
+        // let secrets = client
+        //     .get(secrets_url)
+        //     .send()
+        //     .await
+        //     .map_err(|e| {
+        //         log::error!("Failed to fetch secrets: {:?}", e);
+        //         io::Error::new(
+        //             io::ErrorKind::InvalidInput,
+        //             format!("Failed to fetch secrets: {:?}", e),
+        //         )
+        //     })?
+        //     .text()
+        //     .await
+        //     .map_err(|e| {
+        //         log::error!("Failed to read secrets: {:?}", e);
+        //         io::Error::new(
+        //             io::ErrorKind::InvalidInput,
+        //             format!("Failed to read secrets: {:?}", e),
+        //         )
+        //     })?;
+        // log::info!("Secrets response for server {:?}: {:?}", server, secrets);
     }
 
     // // Configure the server using rustls
