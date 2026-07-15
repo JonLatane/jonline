b7095505c8df6d89c4881a077d2ea46f5f93e270
Jon Latane <jonlatane@gmail.com>
2026-07-15 16:15:59 -0400

    remove stray .elm-spa

--- .elm-spa/defaults/Pages/NotFound.elm
+++
@@ -1,8 +0,0 @@
-module Pages.NotFound exposing (view)
-
-import View exposing (View)
-
-
-view : View msg
-view =
-    View.placeholder "Page not found."
--- .elm-spa/generated/Gen/Msg.elm
+++
@@ -1,8 +0,0 @@
-module Gen.Msg exposing (Msg(..))
-
-import Gen.Params.NotFound
-import Pages.NotFound
-
-
-type Msg = None
-
