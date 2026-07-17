23730c6dfd8bfc2dc1f4721c8f1bde0dc4b06b3f
Jon Latane <jonlatane@gmail.com>
2026-07-17 11:10:50 -0400

    MediaViewerPanel: click image to close, keyboard arrow key nav

--- frontends/elm-spa/src/Shared.elm
+++ frontends/elm-spa/src/Shared.elm
@@ -589,6 +589,7 @@ subscriptions _ model =
         , Sub.map AccountsPanelMsg (AccountsPanel.subscriptions model.accountsPanel)
         , Sub.map FederatedAuthMsg FederatedAuth.subscriptions
         , Sub.map StarredPostsPanelMsg (StarredPostsPanel.subscriptions model.starredPostsPanel)
+        , Sub.map MediaViewerPanelMsg (MediaViewerPanel.subscriptions model.mediaViewerPanel)
         , if model.starredPostsPanel.showStarredPostsPanel then
             Time.every 1500 (\_ -> StarredPostsPanelMsg StarredPostsPanel.PollStarredPosts)
 
