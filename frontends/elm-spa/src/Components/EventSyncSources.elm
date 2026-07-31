5f330694e8d6e24b8debcb615c8c639ef0695c71
Jon Latane <jonlatane@gmail.com>
2026-07-30 23:21:29 -0400

    add event sync every 1 minute

--- frontends/elm-spa/src/Components/EventSyncSources.elm
+++ frontends/elm-spa/src/Components/EventSyncSources.elm
@@ -108,7 +108,8 @@ matches what was asked for: 5/15/30 minutes, 1/4/8 hours, 1 day.
 -}
 intervalOptions : List ( Int, String )
 intervalOptions =
-    [ ( 300, "5 minutes" )
+    [ ( 60, "1 minute" )
+    , ( 300, "5 minutes" )
     , ( 900, "15 minutes" )
     , ( 1800, "30 minutes" )
     , ( 3600, "1 hour" )
