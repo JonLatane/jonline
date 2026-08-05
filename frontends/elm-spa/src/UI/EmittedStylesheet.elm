379043ff8a9f46b79cf447d1ef57b47fad6a8a43
Jon Latane <jonlatane@gmail.com>
2026-08-05 12:16:34 -0400

    minor tweak

--- frontends/elm-spa/src/UI/EmittedStylesheet.elm
+++ frontends/elm-spa/src/UI/EmittedStylesheet.elm
@@ -88,7 +88,7 @@ mainFrontendServerRules theme accountsPanel =
         [ "a { color: " ++ theme.primaryAnchorColor ++ "; }\n"
         , ".switch input:checked + .slider { background: " ++ theme.primaryAnchorColor ++ "; }\n"
         , ".account-row" ++ mainHostSelector ++ " .switch input:checked + .slider { background: " ++ theme.navAnchorColor ++ "; }\n"
-        , ".nav-link:hover { background-color:" ++ theme.navColor ++ "BB; }\n"
+        , ".nav-link:hover { background-color:" ++ theme.navColor ++ "88; }\n"
         ]
 
 
